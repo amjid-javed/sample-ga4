c4129d5c2a8fc857ccf90227403660f090c2fd7d
Amjid Javed <amjid.javed@cartlow.com>
2023-11-17 14:36:41 +0500

    logo click

--- src/App.js
+++ src/App.js
@@ -7,25 +7,19 @@ function App() {
 
   ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search, title: "Home Page" });
 
-  // const logoClick = (value) =>{
-  //   ReactGA.event("add_payment_info",value);
-  // }
-
-  const handleClick = (event, message) => {
-    // 👇️ refers to the image element
+  const tagClick = (event, value) =>{
     console.log(event.target);
 
     console.log(message);
-
-    console.log('Image clicked');
-  };
+    ReactGA.event("add_payment_info",value);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" onClick={logoClick({name: 'Test Logo', currency: 'USD', value: 7.8, items:[{item_id:'1234214', item_name: 'test logo name'}]})}/> */}
-        <img src={logo} className="App-logo" alt="logo" onClick={(event) => handleClick(event, 'hello')}/>
-        <p onClick={(event) => handleClick(event, 'hello')} >
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p onClick={(event) => tagClick(event, {name: 'Test Logo', currency: 'USD', value: 7.8, items:[{item_id:'1234214', item_name: 'test logo name'}]})} >
           Sample site to test GA4 (logo click)
         </p>
         <a
