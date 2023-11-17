e30b7377dc9a5547e02d21fb15635abcbec253ad
Amjid Javed <amjid.javed@cartlow.com>
2023-11-17 14:17:51 +0500

    logo click

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ function App() {
   ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search, title: "Home Page" });
 
   const logoClick = (value) =>{
-    console.log('logo click')
+    console.log('logo click');
+    alert('clicked');
     ReactGA.event("add_payment_info",value);
   }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" onClick={()=>logoClick({name: 'Test Logo'})}/>
+        <img src={logo} className="App-logo" alt="logo" onClick={()=>logoClick({name: 'Test Logo', currency: 'USD', value: 7.8, items:[{item_id:'1234214', item_name: 'test logo name'}]})}/>
         <p>
-          Sample site to test GA4
+          Sample site to test GA4 (logo click)
         </p>
         <a
           className="App-link"
