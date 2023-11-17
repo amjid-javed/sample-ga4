d9f0da35a5b97b6f2dec0af631502b6e65256b7f
Amjid Javed <amjid.javed@cartlow.com>
2023-11-17 11:25:13 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sample site to test GA4
         </p>
         <a
           className="App-link"
